fix: update SNR parameter handling and improve signal normalization in main function

ejercicios/TpFinal/lib/channel/channel.py
@@ -19,12 +19,12 @@ class Channel:
     #  @param fs_MHz Sampling frequency in MHz.
     #  @param f0_MHz Carrier or cutoff frequency in MHz.
     #  @param phase Phase shift in degrees for 'delta' channel. Default is 0.
-    #  @param snr_dB Signal-to-noise ratio in dB. Default is 0 (no noise).
+    #  @param snr_dB Signal-to-noise ratio in dB. Default is None (no noise).
     #  @param kwargs Additional parameters:
     #         - 'samples': number of samples for delta channel.
     #         - 'order': FIR filter order for 'fir' channel.
     #  @raises ValueError If required parameters are missing or invalid.
-    def __init__(self, channel_type, fs_MHz, f0_MHz, phase=0, snr_dB: float = 0, **kwargs):
+    def __init__(self, channel_type, fs_MHz, f0_MHz, phase=0, snr_dB: float | None = None, **kwargs):
         if channel_type == 'delta':
             samples = kwargs.get('samples')
             if samples is None:
@@ -46,6 +46,9 @@ class Channel:
     def add_awgn(self, signal: np.ndarray) -> np.ndarray:
         if not isinstance(signal, np.ndarray):
             raise ValueError("The signal must be a numpy array.")
+        if self._snr_dB is None:
+            return signal
+
         signal_power = np.mean(np.abs(signal)**2)
         snr_linear = 10**(self._snr_dB / 10)
         noise_power = signal_power / snr_linear
@@ -104,6 +107,6 @@ class Channel:
     #  @raises ValueError If value is not a number.
     @snr_dB.setter
     def snr_dB(self, value):
-        if not isinstance(value, (int, float)):
-            raise ValueError("The 'snr_dB' argument must be a numeric value.")
+        if not isinstance(value, (int, float, type(None))):
+            raise ValueError("The 'snr_dB' argument must be a numeric value or None.")
         self._snr_dB = value

ejercicios/TpFinal/lib/pulse_generator/pulse_generator.py
@@ -35,21 +35,21 @@ class PulseGenerator:
                 raise ValueError(f"Invalid pulse type: {type}. Use 'sq', 'sin', 'tr', 'rc', or 'rrc'.")
 
         return pulse_shape, len(pulse_shape)
-            
+
     def square_pulse(self, fs_MHz, f0_MHz, samples):
         t_us = self.time_vector(fs_MHz, samples)
         signal = np.sign(np.sin(2 * np.pi * (f0_MHz/2) * t_us))
         signal[signal < 0] = 0
         return signal
-    
+
     def sine_pulse(self, fs_MHz, f0_MHz, samples):
         t_us = self.time_vector(fs_MHz, samples)
         return np.sin(2 * np.pi * (f0_MHz/2) * t_us)
-    
+
     def triangular_pulse(self, fs_MHz, f0_MHz, samples):
         t_us = self.time_vector(fs_MHz, samples)
         return (2 / np.pi) * np.arcsin(np.sin(2 * np.pi * (f0_MHz/2) * t_us))
-    
+
     def raised_cosine_pulse(self, fs_MHz, f0_MHz, beta, samples):
         t = np.arange(-3 * samples + 1,  3 * samples + 1) / fs_MHz
         T0=1/f0_MHz
@@ -60,7 +60,7 @@ class PulseGenerator:
         signal[t==T0/2/beta] = np.pi/4/T0*np.sinc(1/2/beta)
         signal[t==-T0/2/beta] = np.pi/4/T0*np.sinc(1/2/beta)
         return signal
-    
+
     def root_raised_cosine_pulse(self, fs_MHz, f0_MHz, beta, samples):
         t = np.arange(-3 * samples + 1,  3 * samples + 1) / fs_MHz
         T0=1/f0_MHz
@@ -73,11 +73,11 @@ class PulseGenerator:
         signal [t==T0/4/beta] = beta/T0/np.sqrt(2)*((1+2/np.pi)*np.sin(np.pi/4/beta)+(1-2/np.pi)*np.cos(np.pi/4/beta))
         signal [t==-T0/4/beta] = beta/T0/np.sqrt(2)*((1+2/np.pi)*np.sin(np.pi/4/beta)+(1-2/np.pi)*np.cos(np.pi/4/beta))
         return signal
-    
+
     @staticmethod
     def time_vector(fs_MHz, samples):
         return np.arange(1, samples + 1) / fs_MHz
-    
+
     @property
     def n_pulse(self):
         return self._n_pulse
@@ -93,4 +93,3 @@ class PulseGenerator:
     @property
     def n_fir(self):
         return self._n_fir
-    

ejercicios/TpFinal/tp_final.py
@@ -68,7 +68,7 @@ def main():
 
     ## @var snr_dB_array
     #  @brief List of SNR values in decibels to be tested.
-    snr_dB_array = [30, 10, 5, 0]
+    snr_dB_array = [None, 10, 5, 0]
 
     ## @var error_avg
     #  @brief Array to store the average error percentage for each combination of PLL parameters and SNR.
@@ -130,7 +130,15 @@ def main():
                         plt_process = Plotter(num_stages=3, fs_MHz=fs_MHz)
                         plt_process.add_plot(x, d, title="Signal")
                         plt_process.add_plot(c, d, title="Signal after Channel")
-                        plt_process.add_plot(demodulator.dis['y_mf'], demodulator.dis['y_mf']*demodulator.dis['en_sample']/abs(demodulator.dis['y_mf']), title="Signal after match filter")
+                        y_mf = demodulator.dis['y_mf']
+                        en_sample = demodulator.dis['en_sample']
+                        abs_y_mf = np.abs(y_mf)
+                        EPSILON = 1e-10
+                        normalized_signal = np.zeros_like(y_mf)
+                        valid_indices = (abs_y_mf > EPSILON)
+                        normalized_signal[valid_indices] = y_mf[valid_indices] / abs_y_mf[valid_indices]
+                        safe_division = normalized_signal * en_sample
+                        plt_process.add_plot(y_mf, safe_division, title="Signal after match filter")
                         bit_received = np.unpackbits(hat_bytes)
                         print(f"Errors: {errores}, Len diff: {len_diff}, Error %: {error_pct:.2f}%")
                         plt_bytes = Plotter(num_stages=1, fs_MHz=fs_MHz)